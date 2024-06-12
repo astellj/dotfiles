3d92137acf6ee4e6a64d17016cac444f436caba0
James Astell <mrjamerjamer@gmail.com>
2024-06-12 10:43:36 +0100

    add missing projects to navigation

--- .zsh/custom/navigation-scripts.zsh
+++ .zsh/custom/navigation-scripts.zsh
@@ -2,7 +2,7 @@
 
 go() {
     local selected_option
-    selected_option=$(echo "vault\ndotfiles\ndev\npersonal\nplay\nplay10\nplay12\nplay13\nplay17\nplay18\naccord\naccord-newskin\nsoeps\nt2channelhub\naspire2\nccep\nknauf\nportoftyne\nstada\nstrath\nucl\nyork\natm\ngoodwood\nnhm\nsolent\nstepan\ntarget-verature\nfxplus\nsibelco\nverature_plus\ndhl\nforgemasters\nheresafe\nfusion21\njetset" | fzf --height 50% --ansi --no-multi --preview-window right:65%)
+    selected_option=$(echo "vault\ndotfiles\ndev\npersonal\nplay\nplay10\nplay12\nplay13\nplay17\nplay18\naccord\naccord-newskin\nsoeps\nt2channelhub\naspire2\nccep\nknauf\nportoftyne\nstada\nstrath\nucl\nyork\natm\ngoodwood\nnhm\nsolent\nstepan\ntarget-verature\nfxplus\nsibelco\nverature_plus\ndhl\nforgemasters\nheresafe\nfusion21\njetset\nncc\nwellcome\ncollins\nornua" | fzf --height 50% --ansi --no-multi --preview-window right:65%)
 
     case "$selected_option" in
          vault)
@@ -44,7 +44,7 @@ go() {
          soeps|t2channelhub)
             cd ~/Developer/play/play12/$selected_option
             ;;
-         aspire2|ccep|knauf|portoftyne|stada|strath|ucl|york|atm|goodwood|nhm|solent|stepan|target-verature|fxplus|sibelco)
+         aspire2|ccep|knauf|portoftyne|stada|strath|ucl|york|atm|goodwood|nhm|solent|stepan|target-verature|fxplus|sibelco|ncc|wellcome|collins|ornua)
             cd ~/Developer/play/play13/$selected_option
             ;;
          verature_plus|dhl|forgemasters)
