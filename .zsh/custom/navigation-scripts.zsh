a68f6b222b15bf6b4e7f29530b60cc46f52ade45
James Astell <mrjamerjamer@gmail.com>
2025-06-13 16:25:03 +0100

    general update & fix starship command timeout error

--- .zsh/custom/navigation-scripts.zsh
+++ .zsh/custom/navigation-scripts.zsh
@@ -2,7 +2,7 @@
 
 go() {
     local selected_option
-    selected_option=$(echo "vault\ndotfiles\ndev\npersonal\nplay\nplay10\nplay12\nplay13\nplay17\nplay18\naccord\naccord-newskin\nsoeps\nt2channelhub\naspire2\nccepwf\nknauf\nportoftyne\nstada\nstrath\nucl\nyork\natm\ngoodwood\nnhm\nsolent\nstepan\ntarget-verature\nfxplus\nsibelco\nverature_plus\ndhl\nforgemasters\nportal\nfusion21\njetset\nncc\nwellcome\ncollins\nornua\nccep-gatehouse-trials\ne6\nmorrisons\nmasonite\nfelixstowe\ngcu\nwifinity\ninspinia" | fzf --height 50% --ansi --no-multi --preview-window right:65%)
+    selected_option=$(echo "vault\ndotfiles\ndev\npersonal\nplay\nplay10\nplay12\nplay13\nplay17\nplay18\naccord\naccord-newskin\nsoeps\nt2channelhub\naspire2\nccepwf\nknauf\nportoftyne\nstada\nstrath\nucl\nyork\natm\ngoodwood\nnhm\nsolent\nstepan\ntarget-verature\nfxplus\nsibelco\ndemo\ndhl\nforgemasters\nportal\nfusion21\njetset\nncc\nwellcome\ncollins\nornua\nccep-gatehouse-trials\ne6\nmorrisons\nmasonite\nfelixstowe\ngcu\nwifinity\ninspinia" | fzf --height 50% --ansi --no-multi --preview-window right:65%)
 
     case "$selected_option" in
          vault)
@@ -47,7 +47,7 @@ go() {
          aspire2|ccepwf|knauf|portoftyne|stada|strath|ucl|york|atm|goodwood|nhm|solent|stepan|target-verature|fxplus|sibelco|ncc|wellcome|collins|ornua|ccep-gatehouse-trials)
             cd ~/Developer/play/play13/$selected_option
             ;;
-         verature_plus|dhl|forgemasters|e6|morrisons|masonite|felixstowe|gcu|wifinity)
+         demo|dhl|forgemasters|e6|morrisons|masonite|felixstowe|gcu|wifinity)
             cd ~/Developer/play/play17/$selected_option
             ;;
          portal)
